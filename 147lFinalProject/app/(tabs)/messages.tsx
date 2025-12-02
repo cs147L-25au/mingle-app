074229babdad8d065c228a240cdec2ac907aaf64
Siena Rojas <sienalr@stanford.edu>
2025-12-02 11:28:34 -0800

    basic messages style implemented

--- 147lFinalProject/app/(tabs)/messages.tsx
+++ 147lFinalProject/app/(tabs)/messages.tsx
@@ -1,9 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Messages() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Messages Tab</Text>
+    <View style={styles.container}>
+      <View style={styles.messageBox}>
+        <View style={styles.boxContent}>
+          <Text style={styles.emojiText}>😊</Text>
+
+          <View style={styles.textArea}>
+            <Text style={styles.groupNameText}>Example Group</Text>
+            <Text style={styles.messageText}>Example Message</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    margin: 5,
+  },
+  messageBox: {
+    flex: 0.15,
+    backgroundColor: "#cdefffff",
+    width: "100%",
+    //borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderRadius: "8%",
+  },
+  boxContent: {
+    flexDirection: "row",
+    marginLeft: "4%",
+    gap: 10,
+  },
+  groupNameText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  messageText: {
+    fontSize: 15,
+  },
+  textArea: {
+    flexDirection: "column",
+    gap: 5,
+  },
+  emojiText: {
+    fontSize: 40,
+  },
+});
